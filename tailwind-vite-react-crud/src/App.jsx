6544d08447edb5ab0e455e89c7e338ee7ef6f9d5
harshita <harshita@logicrays.com>
2025-01-24 12:03:24 +0530

    24jan

--- tailwind-vite-react-crud/src/App.jsx
+++ tailwind-vite-react-crud/src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   <>
     {/* <img src={Image1}/> */}
 
-    <h1 className='font-bold text-red-500'>Hello world!</h1>
+    {/* <h1 className='font-bold text-red-500'>Hello world!</h1>
     <button type="button" className='bg-amber-400 border-2 border-amber-900
      border-solid p-10 cursor-pointer hover:bg-red-600 hover:text-3xl 
-     transition-all 4s ease' onClick={()=>alert("button clicked")}>click me</button>
+     transition-all duration-3000 ease' onClick={()=>alert("button clicked")}>click me</button> */}
     <Outlet/>
   </>
   )
