256389aaeafbeb170cad455099585b954b4ac8ae
harshita <harshita@logicrays.com>
2025-02-20 11:14:37 +0530

    20feb

--- mainproject/JSON_Backend/index.js
+++ mainproject/JSON_Backend/index.js
@@ -1,17 +1,13 @@
-import jsonServer from 'json-server';
-import path from 'path'
-import fs from 'fs'
-import cors from 'cors'
+const jsonServer = require('json-server');
+const path =require('path')
 const server = jsonServer.create();
-
-const __dirname = path.resolve()
+const fs = require('fs')
 const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json')))
 const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.use(cors())
 
 const port = 8080;
 server.listen(port, () => {
